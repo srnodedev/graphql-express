f38d6f364b97bfc610703a777ffc84c79e245c16
srnodedev <117995564+srnodedev@users.noreply.github.com>
2022-11-17 07:28:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express') 
 const { graphqlHTTP } = require('express-graphql')
 const graphql = require('graphql')
 const joinMonster = require('join-monster')
